e742fb5e306cb52a24574caf1bf1aaa29122bebf
tqsang <tranquangsang2702@gmail.com>
2020-10-07 10:32:19 +0700

    Add Couter.js and Couter.html

---
+++ src/components/Couter/Couter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Couter.propTypes = {
+
+};
+
+function Couter(props) {
+    return ( <
+        div >
+
+        <
+        h2 > Hello world < /h2>  <
+        /div>
+    );
+}
+
+export default Couter;
